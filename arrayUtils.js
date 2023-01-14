e0c1592f81d91c5e4d94db681dd1939865933155
Kushagraved <kushagraved@gmail.com>
2023-01-15 00:46:52 +0530

    feat: add feature to check if all digits are same

--- arrayUtils.js
+++ arrayUtils.js
@@ -7,5 +7,27 @@ const computeLargestEven=(input)=>{
     })
     return max_elem===Number.MIN_VALUE ? -1:max_elem;
 }
-console.log(computeLargestEven([1,2,3,4,5]));
-console.log(computeLargestEven([1, 3, 5, 7]));
+// console.log(computeLargestEven([1,2,3,4,5]));
+// console.log(computeLargestEven([1, 3, 5, 7]));
+
+const checkDigitsSame=(input)=>{
+    if(input.toString().length===1){
+        console.log("Consider numbers to have 2 digits or mor");
+    }
+    let temp=input;
+    //let allSame=true;
+    let prev;
+    while(temp>0){
+        let rem=temp%10;
+        if(!prev || prev===rem){
+            prev=rem;
+        }
+        else{
+            return false;
+        }
+        temp=parseInt(temp/10);
+    }
+    return true;
+}
+console.log(checkDigitsSame(22));
+console.log(checkDigitsSame(23));
